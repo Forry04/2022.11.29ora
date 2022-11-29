7d9cc8385f8c897aa330c3a1e3f646e8f549a8d7
Forry04 <ebarni04@gmail.com>
2022-11-29 10:52:08 +0100

    Add project files.

---
+++ 2022.11.29ora/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022._11._29ora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tomb = new int[] { 4, 2, 9, 1, 7, 5 };
+
+           
+
+            Console.ReadKey();
+        }
+    }
+}
