febb2ac1b5aaa72a91fd0f30dd33c87aceb57f38
Forry04 <83726220+Forry04@users.noreply.github.com>
2022-11-29 17:10:53 +0100

    továbbfejleszt beszúrásos rendezés

--- 2022.11.29ora/Program.cs
+++ 2022.11.29ora/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,8 +28,11 @@ namespace _2022._11._29ora
             }
             */
 
+
+
             //buborék rendezés
 
+            /*
             for (int i = tomb.Length; i > 0 ; i--)
             {
                 for (int j = 0; j < i-1; j++)
@@ -41,8 +45,65 @@ namespace _2022._11._29ora
                     }
                 }
             }
+            */
 
+            //órai vége
 
+            //tovább fejlesztett buborék
+
+            /*
+            int i = tomb.Length;
+
+            while (i>=1)
+            {
+                int csere = -1;
+
+                for (int j = 0; j < i-1; j++)
+                {
+                    if (tomb[j] > tomb[j+1])
+                    {
+                        int temp = tomb[j];
+                        tomb[j] = tomb[j + 1];
+                        tomb[j + 1] = temp;
+                        csere = j;
+                    }
+                }
+                i = csere;
+            }
+
+        */
+            //beszúró
+            /*
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                int j = i - 1;
+                while (j>=0 && tomb[j] > tomb[j+1])
+                {
+                    int temp = tomb[j];
+                    tomb[j] = tomb[j + 1];
+                    tomb[j + 1] = temp;
+                    j--;
+
+                }
+            }
+            */
+
+            //továbbfejlesztett beszúró
+            /*
+            for (int i = 1; i < tomb.Length; i++)
+            {
+                int j = i - 1;
+                int temp = tomb[i];
+
+                while (j>=0 && tomb[j] > temp)
+                {
+                    tomb[j + 1] = tomb[j];
+                    j--;
+
+                }
+                tomb[j + 1] = temp;
+            }
+            */
 
             foreach (var item in tomb)
             {
