55d62b9a2fb8682eecefc8aaf3a862db48de7eea
Forry04 <ebarni04@gmail.com>
2022-11-29 11:20:58 +0100

    boburék

--- 2022.11.29ora/Program.cs
+++ 2022.11.29ora/Program.cs
@@ -10,9 +10,43 @@ namespace _2022._11._29ora
     {
         static void Main(string[] args)
         {
-            int[] tomb = new int[] { 4, 2, 9, 1, 7, 5 };
+            int[] tomb = new int[] { 4, 2, 9, 1, 7, 5, 1, 3};
 
-           
+            /*
+            for (int i = 0; i < tomb.Length - 1; i++)
+            {
+                for (int j = i+1; j < tomb.Length; j++)
+                {
+                    if (tomb[i]>tomb[j])
+                    {
+                        int temp = tomb[j];
+                        tomb[j] = tomb[i];
+                        tomb[i] = temp;
+                    }
+                }
+            }
+            */
+
+            //buborék rendezés
+
+            for (int i = tomb.Length; i > 0 ; i--)
+            {
+                for (int j = 0; j < i-1; j++)
+                {
+                    if (tomb[j]>tomb[j+1])
+                    {
+                        int temp = tomb[j];
+                        tomb[j] = tomb[j + 1];
+                        tomb[j + 1] = temp;
+                    }
+                }
+            }
+
+
+            foreach (var item in tomb)
+            {
+                Console.Write($"{item} ");
+            }
 
             Console.ReadKey();
         }
